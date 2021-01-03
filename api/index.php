1e3e22b8363d6517649fd7d6d8e3637f2a004a55
Denyck <samurai.samuka27@gmail.com>
2021-01-03 14:06:06 -0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,11 @@
 <?php
 
-header('content-type: application/json');
-echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
+  header('content-type: application/json');
+  echo json_encode(['$effect' => 'fade', 'red' => 0, 'green' => '255', 'blue' => 0]);
+
+  // $effect = $_POST['efeito'];
+  // $red = $_POST['r'];
+  // $green = $_POST['g'];
+  // $blue = $_POST['b'];
+
+?>
